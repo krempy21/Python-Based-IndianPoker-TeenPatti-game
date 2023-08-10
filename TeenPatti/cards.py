4ba6457dd0b0e173bc4824c2b433fee566bf73e1
krempy21 <rishi2109@vt.edu>
2023-08-10 18:37:54 -0400

    deleted unecessary files

--- TeenPatti/cards.py
+++
@@ -1,8 +0,0 @@
-class Card:
-    def __init__(self, suit, denomination):
-        self.suit = suit
-        self.denomination = denomination
-
-    def __str__(self):
-        return self.denomination["denomination"] + " of " + self.suit
-
